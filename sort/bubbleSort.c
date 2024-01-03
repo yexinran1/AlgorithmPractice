494d2781510bb8b9c32f81731fcf9ca0f9d217d9
yexinran1 <yexinran96@163.com>
2024-01-03 15:31:21 +0800

    [修改]新增判空

--- sort/bubbleSort.c
+++ sort/bubbleSort.c
@@ -15,10 +15,16 @@ int swap(int *val1, int *val2)
 /* 数组作函数参数会自动弱化成指针 */
 int printArray(int *array, int arraySize)
 {
+    int ret = 0;
+    if (array == NULL)
+    {
+        return ret;
+    }
     for (int idx = 0; idx < arraySize; idx++)
     {
         printf("array[%d]:%d\n", idx, array[idx]);
     }
+    return ret; 
 }
 
 /* 时间复杂度 o(n)^2 */
@@ -65,6 +71,10 @@ void bubbleSort02(int *array, int len)
 /* 优化2 */
 void bubbleSort03(int *array, int len)
 {
+    if (array == NULL)
+    {
+        return;
+    }
     int sortedIndex = 0;
     for (int end = len; end > 0; end--)
     {
@@ -88,7 +98,6 @@ void bubbleSort03(int *array, int len)
 int main()
 {
     int array[] = {11, 36, 24, 107, 23, 65};
-
     int len = sizeof(array) / sizeof(array[0]);
 
     bubbleSort02(array, len);
